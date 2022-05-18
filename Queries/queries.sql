c920e86ad23e7c31d3d21405540e88860698c3f1
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-05-18 15:00:50 -0700

    Updated as of 7.3.6

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -183,40 +183,25 @@ SELECT * FROM dept_info;
 
 
 
+
 -- start of 7.3.6, sales wants a list of employees in their dept. for retirement info
-SELECT ce.emp_no,
-ce.first_name,
-ce.last_name,
-d.dept_name
---INTO retirement_info_salesdept
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
--- this is a table of current employees, add the filter with where
-WHERE de.to_date = ('9999-01-01');
-
--- Check the table after refresh
-SELECT * FROM retirement_info_sales_dept;
-
--- same manager now wants list for both sales and development dept. for retirement info
 -- HINT: use IN condition with the WHERE clause
 SELECT ce.emp_no,
 ce.first_name,
 ce.last_name,
 d.dept_name
---INTO retirement_info_sales_develop_dept
-FROM retirement_info as ri
-LEFT JOIN dept_emp as de
-ON ri.emp_no = de.emp_no
--- this is a table of current employees, add the filter with where
-WHERE de.to_date = ('9999-01-01');
+INTO retirement_info_sales_develop_dept
+
+FROM current_emp as ce
+LEFT JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+LEFT JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name in ('Sales', 'Development');
+	
 
 -- Check the table after refresh
 SELECT * FROM retirement_info_sales_develop_dept;
-
-
-
-
 
 
 
